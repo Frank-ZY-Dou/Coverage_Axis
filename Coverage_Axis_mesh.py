20a5eae6093a3c7fa09e929d74d54ad5b16a20cb
Frank-ZY-Dou <zhiyang0@connect.hku.hk>
2023-02-25 23:13:38 +0800

    CA for meshes are all done.

--- Coverage_Axis_mesh.py
+++ Coverage_Axis_mesh.py
@@ -1,21 +1,27 @@
 # Author: Frank ZY Dou
-
-import numpy as np
-import trimesh
-from scipy.optimize import milp, Bounds, LinearConstraint
-from utils import  save_obj,read_VD
+import os
 import torch
+import trimesh
+import numpy as np
+from tqdm import tqdm
+from utils import  save_obj,read_VD, winding_number
+from scipy.optimize import milp, Bounds, LinearConstraint
 
 
 real_name = '01Ants-12'
 surface_sample_num = 2000
 dilation = 0.02
-max_time_SCP = 90
 # inner_points = "voronoi"
 inner_points = "random"
+max_time_SCP = 100 # in second
+
 
 mesh = trimesh.load('./input/%s.off'%real_name)
 point_set = trimesh.sample.sample_surface(mesh, surface_sample_num)
+
+mesh_faces = np.array(mesh.faces)
+mesh_vertices = np.array(mesh.vertices)
+point_set = np.array(point_set[0])
 
 if inner_points == "voronoi":
     medial_path ='_VD.txt'
@@ -27,32 +33,61 @@ if inner_points == "voronoi":
 
 else:
     print("Generating random samples inside the shape...")
-    inner_points = trimesh.sample.volume_mesh(mesh, 30000)
-    '''
-    Note the returned points can be less than requested 10000 samples
-    '''
-    assert len(inner_points) > surface_sample_num * 4, "The number of samples: %d at least 4 times of surface samples"%len(inner_points)
-    inner_points = np.array(inner_points)
-    print(inner_points.shape)
-    input(22)
 
-mesh_faces = np.array(mesh.faces)
-mesh_vertices = np.array(mesh.vertices)
-point_set = np.array(point_set[0])
+    if os.path.exists("./input/%s_random.obj"%real_name):
+        inner_points = trimesh.load("./input/%s_random.obj"%real_name)
+        inner_points = np.array(inner_points.vertices)
+        print("The number of sampled inner candidates: ", len(inner_points))
+    else:
+        print("Randomly Generating inner candidates...")
+        random_sample_number = 500000
+        min_corner = np.amin(np.amin(mesh_vertices, axis=0), axis=0)
+        max_corner = np.amax(np.amax(mesh_vertices, axis=0), axis=0)
+        center = min_corner + max_corner /2
+        min_x = np.min(mesh_vertices[:,0])
+        min_y = np.min(mesh_vertices[:,1])
+        min_z = np.min(mesh_vertices[:,2])
+        max_x = np.max(mesh_vertices[:, 0])
+        max_y = np.max(mesh_vertices[:, 1])
+        max_z = np.max(mesh_vertices[:, 2])
+        P_x = (max_x) * np.random.random((random_sample_number, 1)) * 1.3 + min_x - 0.1
+        P_y = (max_y) * np.random.random((random_sample_number, 1)) *1.3+ min_y - 0.1
+        P_z = (max_z) * np.random.random((random_sample_number, 1))*1.3 + min_z - 0.1
+        P = np.concatenate((P_x, P_y, P_z), axis=1)
+        winding_con = []
+        for i in tqdm(range(0, len(P), 5000)):
+            start = i
+            end = i + 5000
+            winding = winding_number(torch.tensor(P[start:end,:]).cuda().double(), torch.tensor(mesh_vertices).cuda().double(), torch.tensor(mesh_faces).cuda().long())
+            winding_con.append(winding)
+        winding_con = torch.cat(winding_con, dim=0)
+        inner_points = P[winding_con.cpu().numpy() > 0.5]
+        save_obj("./input/%s_random.obj"%real_name, inner_points)
+
+    inner_points_g = torch.tensor(inner_points).cuda().double()
+    point_set_g = torch.tensor(point_set).cuda().double()
+    dist = torch.cdist(inner_points_g, point_set_g, p=2)
+    radius = dist.topk(1, largest=False).values
+    radius = radius + dilation
+
+
+
 
 save_obj("./output/mesh.obj", mesh_vertices, mesh_faces)
 save_obj("./output/mesh_samples_%d.obj"%surface_sample_num, point_set)
 save_obj("./output/inner_points.obj", inner_points)
 
 # Coverage Matrix -> GPU.
-mesh_vertices_g = torch.tensor(point_set).cuda().double()
+point_set_g = torch.tensor(point_set).cuda().double()
 innerpoints_g = torch.tensor(inner_points).cuda().double()
 radius_g = torch.tensor(radius).cuda().double()
 radius_g = radius_g[:,0]
 radius_g = radius_g.unsqueeze(0).repeat(len(point_set), 1)
-D = torch.cdist(mesh_vertices_g, innerpoints_g, p=2)
+D = torch.cdist(point_set_g, innerpoints_g, p=2)
 D = torch.gt(radius_g, D).type(torch.int)
 D = D.cpu().numpy()
+# Done
+
 c = np.ones(len(inner_points))
 options = {"disp": True, "time_limit": max_time_SCP, }
 A,b =  D, np.ones(len(point_set))
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import trimesh
 def read_VD(path):
     points = []
     radius = []
@@ -20,3 +21,99 @@ def save_obj(path, verts, faces=None):
             faces = faces.tolist()
             for ff in faces:
                 f.write('f %d %d %d\n' % (ff[0] + 1, ff[1] + 1, ff[2] + 1))
+
+
+# Codes borrowed from https://gist.github.com/dendenxu/ee5008acb5607195582e7983a384e644#file-moller_trumbore-py-L27
+import torch
+
+
+
+def multi_indexing(index: torch.Tensor, shape: torch.Size, dim=-2):
+    shape = list(shape)
+    back_pad = len(shape) - index.ndim
+    for _ in range(back_pad):
+        index = index.unsqueeze(-1)
+    expand_shape = shape
+    expand_shape[dim] = -1
+    return index.expand(*expand_shape)
+
+def multi_gather(values: torch.Tensor, index: torch.Tensor, dim=-2):
+    # take care of batch dimension of, and acts like a linear indexing in the target dimention
+    # we assume that the index's last dimension is the dimension to be indexed on
+    return values.gather(dim, multi_indexing(index, values.shape, dim))
+
+def winding_number(pts: torch.Tensor, verts: torch.Tensor, faces: torch.Tensor) -> torch.Tensor:
+    """
+    Parallel implementation of the Generalized Winding Number of points on the mesh
+    O(n_points * n_faces) memory usage, parallelized execution
+    1. Project tris onto the unit sphere around every points
+    2. Compute the signed solid angle of the each triangle for each point
+    3. Sum the solid angle of each triangle
+    Parameters
+    ----------
+    pts    : torch.Tensor, (n_points, 3)
+    verts  : torch.Tensor, (n_verts, 3)
+    faces  : torch.Tensor, (n_faces, 3)
+    This implementation is also able to take a/multiple batch dimension
+    """
+    # projection onto unit sphere: verts implementation gives a little bit more performance
+    uv = verts[..., None, :, :] - pts[..., :, None, :]  # n_points, n_verts, 3
+    uv = uv / uv.norm(dim=-1, keepdim=True)  # n_points, n_verts, 3
+
+    # gather from the computed vertices (will result in a copy for sure)
+    expanded_faces = faces[..., None, :, :].expand(*faces.shape[:-2], pts.shape[-2], *faces.shape[-2:])  # n_points, n_faces, 3
+
+    u0 = multi_gather(uv, expanded_faces[..., 0])  # n, f, 3
+    u1 = multi_gather(uv, expanded_faces[..., 1])  # n, f, 3
+    u2 = multi_gather(uv, expanded_faces[..., 2])  # n, f, 3
+
+    e0 = u1 - u0  # n, f, 3
+    e1 = u2 - u1  # n, f, 3
+    del u1
+
+    # compute solid angle signs
+    sign = (torch.cross(e0, e1) * u2).sum(dim=-1).sign()
+
+    e2 = u0 - u2
+    del u0, u2
+
+    l0 = e0.norm(dim=-1)
+    del e0
+
+    l1 = e1.norm(dim=-1)
+    del e1
+
+    l2 = e2.norm(dim=-1)
+    del e2
+
+    # compute edge lengths: pure triangle
+    l = torch.stack([l0, l1, l2], dim=-1)  # n_points, n_faces, 3
+
+    # compute spherical edge lengths
+    l = 2 * (l/2).arcsin()  # n_points, n_faces, 3
+
+    # compute solid angle: preparing: n_points, n_faces
+    s = l.sum(dim=-1) / 2
+    s0 = s - l[..., 0]
+    s1 = s - l[..., 1]
+    s2 = s - l[..., 2]
+
+    # compute solid angle: and generalized winding number: n_points, n_faces
+    eps = 1e-10  # NOTE: will cause nan if not bigger than 1e-10
+    solid = 4 * (((s/2).tan() * (s0/2).tan() * (s1/2).tan() * (s2/2).tan()).abs() + eps).sqrt().arctan()
+    signed_solid = solid * sign  # n_points, n_faces
+
+    winding = signed_solid.sum(dim=-1) / (4 * torch.pi)  # n_points
+
+    return winding
+
+
+def ball_vis(position, r):
+    for i in range(r.shape[0]):
+        mesh = trimesh.load('./assets/sphere_I.obj')
+        mesh.vertices = mesh.vertices * r[i]
+        mesh.vertices = mesh.vertices + position[i]
+        mesh.export('./vis_ball/%04d.obj'%i)
+
+
+
